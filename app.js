605756d42ebe5c7544a793b9e4d64a5206d7b38f
EL BEQQAL Youssef <ELBEQQAL.youssef@gmail.com>
2019-02-20 15:15:08 +0000

    create sample web server

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log(2);
+// Create sample Web Server With nodejs
+
+// Get http module from nodejs APIs
+const http = require('http');
+
+//Create hostname
+const hostname = '127.0.0.1';
+
+// Create PORT
+const port = 500;
+
+// Create server 
+const server = http.createServer((req, res) => {
+    // Create status code
+    res.statusCode = 200;
+
+    // Create header
+    res.setHeader('Content-Type', 'text/plain');
+
+    // End of response server
+    res.end('Hello Nodejs from app.js');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
