e52bc39216d8cdd16e2060f38fadbd775966de09
EL BEQQAL Youssef <ELBEQQAL.youssef@gmail.com>
2019-02-21 13:41:08 +0000

    modified app.js

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
-// Create sample Web Server With nodejs
+// Get Express module
+const express = require('express');
+const app = express();
 
-// Get http module from nodejs APIs
-const http = require('http');
+// Get path module from Nodejs module
+const path = require('path');
 
-//Create hostname
-const hostname = '127.0.0.1';
+// Set static path
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Create PORT
-const port = 500;
-
-// Create server 
-const server = http.createServer((req, res) => {
-    // Create status code
-    res.statusCode = 200;
-
-    // Create header
-    res.setHeader('Content-Type', 'text/plain');
-
-    // End of response server
-    res.end('Hello Nodejs from app.js');
-});
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(500, () => console.log('Server started on  port 500...'));
