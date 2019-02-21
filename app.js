781fc4d6a94f783dc063fbbf1c14136a2e2dad83
EL BEQQAL Youssef <ELBEQQAL.youssef@gmail.com>
2019-02-21 14:01:01 +0000

    Next changes

--- app.js
+++ app.js
@@ -8,4 +8,32 @@ const path = require('path');
 // Set static path
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Get json data
+app.get('/users', (req, res) => {
+    let users = [
+        {
+            name : 'Youssef',
+            lname : 'ELBEQQAl',
+            age : 24
+        },
+
+        {
+            name : 'Fatima',
+            lname : 'ELOUTHMANI',
+            age : 22
+        },
+        {
+            name : 'Ghaya',
+            lname : 'ELBEQQAl',
+            age : 2
+        }
+    ];
+    res.json(users);
+});
+
+// Get PDF file
+app.get('/download', (req, res) => {
+   res.download(path.join(__dirname, '/download/pdf.pdf'));
+});
+
 app.listen(500, () => console.log('Server started on  port 500...'));
